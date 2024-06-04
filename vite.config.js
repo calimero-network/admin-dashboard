2b64eb6cfeea474cabc3b17a841030fe31ff6921
Matej Vukosav <MatejVukosav@users.noreply.github.com>
2024-06-04 14:38:52 +0200

    Test gh pages (#1)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import EnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin/',
-  plugins: [
-    nodePolyfills(),
-    EnvironmentPlugin('all')
-  ],
-})
+  base: "/admin/",
+  build: {
+    outDir: "build",
+  },
+  plugins: [nodePolyfills(), EnvironmentPlugin("all")],
+});
