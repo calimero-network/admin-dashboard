42a865ad84afc8f852f186b64ed6631fabf05a6c
matej.vukosav <matej.vukosav@gmail.com>
2024-06-05 18:37:59 +0200

    fix: add react to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,5 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [nodePolyfills()],
+  plugins: [nodePolyfills(), react()],
 });
