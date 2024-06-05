f34203d1dca6c847b4bf385239dd87c5a7240592
matej.vukosav <matej.vukosav@gmail.com>
2024-06-05 18:25:38 +0200

    fix: set pathSegmentsToKeep to 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bootstrap from "./pages/Bootstrap";
 import Identity from "./pages/Identity";
 import Applications from "./pages/Applications";
