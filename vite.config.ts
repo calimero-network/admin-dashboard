42b51c41c2acec12a63af577c4785bd6641d19ab
matej.vukosav <matej.vukosav@gmail.com>
2024-06-05 17:26:28 +0200

    fix: add / to base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin-dashboard",
+  base: "/admin-dashboard/",
   build: {
     outDir: "build",
   },
