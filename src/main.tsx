c0e1a74c39c08457d5cb6826f52a46087cbe5265
Fran Domovic <fran@calimero.network>
2024-06-05 16:59:19 +0200

    feat: update apps return type

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./styles/index.css";
 import "react-tooltip/dist/react-tooltip.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
