8cbc39827956444de75360930e58ee390994b308
matej.vukosav <matej.vukosav@gmail.com>
2024-06-10 17:08:18 +0200

    refactor: setup

--- src/pages/setup/index.tsx
+++ src/pages/setup/index.tsx
@@ -3,27 +3,18 @@ import SetupModal from "@calimero-is-near/calimero-p2p-sdk/lib/setup/SetupModal"
 import { useNavigate } from "react-router-dom";
 import { getNodeUrl } from "../../utils/node";
 import { setAppEndpointKey } from "../../utils/storage";
-import { styled } from "styled-components";
-
-const FullPageCenter = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  background-color: #111111;
-  justify-content: center;
-  align-items: center;
-`;
+import ContentWrapper from "../../components/login/ContentWrapper";
 
 export default function SetupPage() {
   const navigate = useNavigate();
 
   return (
-    <FullPageCenter>
+    <ContentWrapper>
       <SetupModal
         successRoute={() => navigate("/auth")}
         getNodeUrl={getNodeUrl}
         setNodeUrl={setAppEndpointKey}
       />
-    </FullPageCenter>
+    </ContentWrapper>
   );
 }
