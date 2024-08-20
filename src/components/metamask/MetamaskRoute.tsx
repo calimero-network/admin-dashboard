4cdae3cc5990beb0c4c59a16e1e199314aa3b1e9
Fran Domovic <fran@calimero.network>
2024-08-20 10:59:01 +0200

    fix: add translation

--- src/components/metamask/MetamaskRoute.tsx
+++ src/components/metamask/MetamaskRoute.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import { MetaMaskUIProvider } from '@metamask/sdk-react-ui';
 import { Outlet } from 'react-router-dom';
+import translations from "../../constants/en.global.json";
 
 export default function MetamaskRoute() {
+  const t = translations.useMetamask;
+
   return (
     <MetaMaskUIProvider
       sdkOptions={{
         dappMetadata: {
-          name: 'admin-dashboard',
+          name: t.applicationNameText,
         },
         checkInstallationOnAllCalls: true,
       }}
