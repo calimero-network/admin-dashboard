451a67cc1929b84c9ea3ebf7c0173f28a1c2f98d
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2024-08-22 10:15:25 +0200

    feat: add apps list copy clipboard (#55)

--- src/components/applications/ApplicationRowItem.tsx
+++ src/components/applications/ApplicationRowItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { truncateHash } from '../../utils/displayFunctions';
 import { Application } from '../../api/dataSource/NodeDataSource';
+import { ClipboardDocumentIcon } from '@heroicons/react/24/solid';
 
 interface ApplicationRowItemProps {
   $hasBorders: boolean;
@@ -47,6 +48,16 @@ const RowItem = styled.div<ApplicationRowItemProps>`
   .read {
     color: #9c9da3;
   }
+
+  .copy-icon {
+    height: 1.5rem;
+    width: 1.5rem;
+    color: #9c9da3;
+    cursor: pointer;
+  }
+  .copy-icon:hover {
+    color: #fff;
+  }
 `;
 
 export default function applicationRowItem(
@@ -55,6 +66,12 @@ export default function applicationRowItem(
   count: number,
   onRowItemClick?: (id: string) => void,
 ): JSX.Element {
+  const copyToClippboard = (text: string) => {
+    navigator.clipboard.writeText(text).catch((err) => {
+      console.error('Failed to copy text to clipboard: ', err);
+    });
+  };
+
   return (
     <RowItem key={item.id} $hasBorders={id === count}>
       <div
@@ -63,7 +80,13 @@ export default function applicationRowItem(
       >
         {item.name}
       </div>
-      <div className="row-item read">{truncateHash(item.id)}</div>
+      <div className="row-item read">
+        <ClipboardDocumentIcon
+          className="copy-icon"
+          onClick={() => copyToClippboard(item.id)}
+        />
+        <span>{truncateHash(item.id)}</span>
+      </div>
       <div className="row-item read">{item.version}</div>
       <div className="row-item read">-</div>
     </RowItem>
