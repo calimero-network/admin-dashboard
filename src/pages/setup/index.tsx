2b0c2fa6d8810babb8f8df1dd5aa3bc18853697e
Fran Domovic <fran@calimero.network>
2025-06-04 11:56:25 +0200

    feat: add node url from query

--- src/pages/setup/index.tsx
+++ src/pages/setup/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { clearStorage, setAppEndpointKey } from '../../utils/storage';
 import ContentWrapper from '../../components/login/ContentWrapper';
 import { SetupModal } from '../../components/setup/SetupModal';
@@ -7,10 +7,16 @@ import { getNodeUrl } from '../../utils/node';
 
 export default function SetupPage() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     clearStorage();
-  }, []);
+    const params = new URLSearchParams(location.search);
+    const nodeUrl = params.get('node-url');
+    if (nodeUrl) {
+      setAppEndpointKey(nodeUrl);
+    }
+  }, [location.search]);
 
   return (
     <ContentWrapper>
