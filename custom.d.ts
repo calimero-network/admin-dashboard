fdb81b4fbdb1ddaa711ea29bdef84d6dbd4fbb13
fran domovic <fran@calimero.network>
2026-01-08 15:59:07 +0100

    fix: svg load + logout error

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,8 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.svg?url' {
+  const content: string;
+  export default content;
+}
